947a4db6382b2ca207f9617b91ecfd2778f1e27f
seungsoo <seung_soos@naver.com>
2024-08-08 23:11:45 +0900

    - chap 1 @Configuration 주석처리

--- src/main/java/org/example/toby_spring/chap1/sec5/paragraph1/DaoFactory.java
+++ src/main/java/org/example/toby_spring/chap1/sec5/paragraph1/DaoFactory.java
@@ -4,10 +4,10 @@ package org.example.toby_spring.chap1.sec5.paragraph1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class DaoFactory {
 
-    @Bean
+//    @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
